111b13aa4a9ba06791c42140d9335957bcad1e58
Matt Kirby <MattJKirby@outlook.com>
2024-10-05 18:30:23 +0100

    semi test

--- ga/app/app_test.go
+++ ga/app/app_test.go
@@ -17,3 +17,39 @@ func TestRegisterDef(t *testing.T){
 		}
 	})
 }
+
+func TestGetActionDef(t *testing.T){
+	app := NewApp()
+	def := action.ActionDefinition{}
+	app.RegisterActionDef(&def)
+
+	result, _ := app.GetActionDef(def.ActionType())
+
+	t.Run("Test get def", func(t *testing.T) {
+		if result != &def {
+			t.Errorf("Error getting definition: expected %v, got %v", &def, result)
+		}
+	})
+}
+
+type myAction struct {}
+func (ma myAction) Execute(){}
+
+func TestNewAction(t *testing.T){
+	app := NewApp()
+	def := action.ActionDefinition{}
+	app.RegisterActionDef(&def)
+
+	expected := action.NewAction[myAction](&def)
+	result, err := NewAction[myAction](def.ActionType())(app)
+
+	t.Run("Test making action", func(t *testing.T) {
+		if *result != *expected {
+			t.Errorf("error instatiating action: expected %v, got %v", *expected, *result)
+		}
+
+		if err != nil {
+			t.Errorf("error instatiating action: err not expected %v", err)
+		}
+	})
+}
