fcd59e62c4c8d72e33f113ffc212b8c0d1fdd9b0
Matt Kirby <MattJKirby@outlook.com>
2024-10-27 15:12:48 +0000

    GoActionInternals

--- ga/action/goAction.go
+++ ga/action/goAction.go
@@ -1,7 +1,19 @@
 package action
 
+import "go-actions/ga/action/instance"
+
 type GoActionConstructor[T GoAction] func() *T
 
 type GoAction interface {
 	Execute()
+}
+
+type GoActionInternals struct {
+	instance *instance.ActionInstance
+}
+
+func NewGoActionInternals(actionName string) *GoActionInternals {
+	return &GoActionInternals{
+		instance: instance.NewActionInstance(actionName),
+	}
 }
---
+++ ga/action/goAction_test.go
@@ -0,0 +1,16 @@
+package action
+
+import (
+	"go-actions/ga/action/instance"
+	"go-actions/ga/cr/asserts"
+	"testing"
+)
+
+func TestNewGoActionInternals(t *testing.T) {
+	internals := NewGoActionInternals("test")
+	expectedInstance := instance.NewActionInstance("test")
+	expectedInstance.ActionUid = internals.instance.ActionUid
+	t.Run("test create new internals", func(t *testing.T) {
+		asserts.Equals(t, expectedInstance, internals.instance)
+	})
+}
