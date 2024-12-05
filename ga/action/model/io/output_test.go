10450a9e3da395b2df269401e595aac659398fc6
Matt Kirby <MattJKirby@outlook.com>
2024-12-05 00:35:45 +0000

    Test Marshal with inputs

--- ga/action/model/io/output_test.go
+++ ga/action/model/io/output_test.go
@@ -2,6 +2,7 @@ package io
 
 import (
 	"encoding/json"
+	"fmt"
 	"go-actions/ga/action/model/io/reference"
 	"go-actions/ga/cr/asserts"
 	"testing"
@@ -29,5 +30,16 @@ func TestMarshal(t *testing.T) {
 		marshalled, err := json.Marshal(output)
 		asserts.Equals(t, nil, err)
 		asserts.Equals(t, `{"name":"o","id":"uid__Output:o","inputs":[]}`, string(marshalled))
+	})
+
+	t.Run("with inputs", func(t *testing.T) {
+		inputRef := reference.NewInputReference("other uid", "output")
+		output.AssignInputReference(inputRef)
+		marshalledInputRef, _ := json.Marshal(inputRef)
+		marshalledOutput, err := json.Marshal(output)
+
+		expected := fmt.Sprintf(`{"name":"o","id":"uid__Output:o","inputs":[%s]}`, string(marshalledInputRef))
+		asserts.Equals(t, nil, err)
+		asserts.Equals(t, expected, string(marshalledOutput))
 	})
 }
