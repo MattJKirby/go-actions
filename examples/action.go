a73b8fa20567c25097cf7c54cbb097365bfaa069
Matt Kirby <MattJKirby@outlook.com>
2024-12-15 13:46:02 +0000

    fixed model example

--- examples/action.go
+++ examples/action.go
@@ -22,7 +22,7 @@ type ExampleAction struct {
 
 func NewExampleAction(instance action.ActionInstance) *ExampleAction {
 	return &ExampleAction{
-		IntegerParameter: model.Parameter("intParam", 10)(instance),
+		IntegerParameter: model.Parameter("intParam", 10)(instance.ActionModel),
 		StringParameter:  model.Parameter("strParam", "test")(instance.ActionModel),
 		Input:            model.Input("input", true)(instance.ActionModel),
 		Output:           model.Output("output")(instance.ActionModel),
