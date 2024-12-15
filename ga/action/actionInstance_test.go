baf9b904f675a1d7a3f5d1f076f7c10e9cff52b9
Matt Kirby <MattJKirby@outlook.com>
2024-12-15 01:00:21 +0000

    deleted unused test code

--- ga/action/actionInstance_test.go
+++ ga/action/actionInstance_test.go
@@ -11,8 +11,6 @@ func TestNewActionInstance(t *testing.T) {
 	instance := NewActionInstance("test")
 	model := model.NewActionModel("test", &config.ModelConfig{})
 	model.ActionUid = instance.ActionUid
-	// model.Inputs = resources.NewResourceStore[io.Input]()
-	// model.Outputs = resources.NewResourceStore[io.Output]()
 
 	asserts.Equals(t, model, instance.ActionModel)
 }
