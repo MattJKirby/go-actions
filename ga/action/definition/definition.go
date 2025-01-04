764a3a215e2e6cb849eb2adb6971648ce8f67955
Matt Kirby <MattJKirby@outlook.com>
2025-01-04 13:31:37 +0100

    fmt

--- ga/action/definition/definition.go
+++ ga/action/definition/definition.go
@@ -8,17 +8,17 @@ import (
 type ActionDefinition[T action.GoAction, P action.GoActionProps] struct {
 	*action.GoActionRegistration[T, P]
 	*ActionTypeDefinition
-	Name           string
-	TypePath       string
+	Name     string
+	TypePath string
 }
 
 func NewActionDefinition[T action.GoAction, P action.GoActionProps](reg *action.GoActionRegistration[T, P]) (*ActionDefinition[T, P], error) {
 	typeDef := TypeDefinitionFromConstructor(reg.Constructor)
 
 	return &ActionDefinition[T, P]{
-		GoActionRegistration:   reg,
+		GoActionRegistration: reg,
 		ActionTypeDefinition: typeDef,
-		Name:           utils.TypeName(typeDef.ActionType),
-		TypePath:       utils.TypePath(typeDef.ActionType),
+		Name:                 utils.TypeName(typeDef.ActionType),
+		TypePath:             utils.TypePath(typeDef.ActionType),
 	}, nil
 }
