2fc5f64f045b7b6aad0c1b5118c93f2a32cdf31a
Matt Kirby <MattJKirby@outlook.com>
2025-01-06 22:49:11 +0100

    test apply constructor

--- ga/action/executable/action_test.go
+++ ga/action/executable/action_test.go
@@ -2,18 +2,32 @@ package executable
 
 import (
 	"go-actions/ga/action/definition"
+	"go-actions/ga/action/model"
+	"go-actions/ga/cr/asserts"
 	ta "go-actions/ga/testing/testActions"
 	"testing"
 )
 
 func TestNewAction(t *testing.T) {
-
 	reg := ta.GenerateActionValidRegistration()
 	def, _ := definition.NewActionDefinition(&reg)
 
-	acn := NewAction(*def)
+	action := NewAction(*def)
 
-	if acn.Instance == nil {
+	if action.Instance == nil {
 		t.Errorf("invalid action: instance expected but got nil")
 	}
+}
+
+func TestApplyConstructorNoProps(t *testing.T){
+	reg := ta.GenerateActionValidRegistration()
+	def, _ := definition.NewActionDefinition(&reg)
+	action := NewAction(*def)
+	
+	instance := *action.Instance
+	model.Parameter("param", 10)(instance.Model)
+
+	applyConstructor(*def, &instance, nil)
+
+	asserts.Equals(t, instance, *action.Instance)
 }
