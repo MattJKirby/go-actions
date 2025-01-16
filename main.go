a9765e82a5d23f6b1ffa82db7f962a1e22943529
Matt Kirby <MattJKirby@outlook.com>
2025-01-16 21:29:16 +0000

    uncomment getreg

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"go-actions/examples"
 	"go-actions/ga"
 )
 
 func main() {
-	// definition, _ := ga.GetActionRegistration[examples.ExampleAction, examples.ExampleActionProps]()
-	// action, _ := ga.GetAction[examples.ExampleAction, examples.ExampleActionProps]()
-	// fmt.Println(definition)
-	// fmt.Println(action)
+	definition, _ := ga.GetActionRegistration[examples.ExampleAction, examples.ExampleActionProps]()
+	action, _ := ga.GetAction[examples.ExampleAction, examples.ExampleActionProps]()
+	fmt.Println(definition)
+	fmt.Println(action)
 
 	flow := ga.NewFlow()
 	ex1 := examples.NewExampleAction(flow, &examples.ExampleActionProps{
