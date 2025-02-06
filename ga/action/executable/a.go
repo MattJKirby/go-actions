a7b28b6ecf339ffbbe7aca0bb680fb0c3b17521e
Matt Kirby <MattJKirby@outlook.com>
2025-02-06 00:01:02 +0000

    Executable instance

--- ga/action/executable/a.go
+++ ga/action/executable/a.go
@@ -12,3 +12,12 @@ type ExecutableAction struct {
 func NewExecutableAction(typeDef *definition.ActionTypeDefinition, props *action.GoAction) *ExecutableAction {
 	return &ExecutableAction{}
 }
+
+func newExecutableInstance(typeDef *definition.ActionTypeDefinition) *action.ActionInstance{
+	instance := action.NewActionInstance(typeDef.TypeName)
+	props := typeDef.NewDefaultProps()
+	ctor := typeDef.NewConstructor()
+
+	ctor(instance, props)
+	return instance
+}
--- ga/action/executable/a_test.go
+++ ga/action/executable/a_test.go
@@ -1,14 +1,20 @@
 package executable
 
 import (
+	"go-actions/ga/action"
 	"go-actions/ga/action/definition"
+	"go-actions/ga/cr/asserts"
 	ta "go-actions/ga/testing/testActions"
 	"testing"
 )
 
-func TestNewExecutableAction(t *testing.T) {
+func defHelper() *definition.ActionDefinition[ta.ActionValidEmpty, ta.ActionValidEmptyProps]{
 	reg := ta.GenerateActionValidEmptyRegistration()
-	def := definition.NewActionDefinition(&reg)
+	return definition.NewActionDefinition(&reg)
+}
+
+func TestNewExecutableAction(t *testing.T) {
+	def := defHelper()
 	typeDef := def.GetTypeDefinition()
 
 	action := NewExecutableAction(typeDef, nil)
@@ -16,4 +22,19 @@ func TestNewExecutableAction(t *testing.T) {
 	if action == nil {
 		t.Errorf("invalid action: instance expected but got nil")
 	}
+}
+
+func TestNewExeutableInstance(t *testing.T) {
+	def := defHelper()
+	typeDef := def.GetTypeDefinition()
+
+	expectedInst := action.NewActionInstance(def.TypeName)
+	expectedInst.Model.ActionUid = ""
+	def.Constructor(expectedInst, ta.ActionValidEmptyProps{})
+	
+	inst := newExecutableInstance(typeDef)
+	inst.Model.ActionUid = ""
+
+
+	asserts.Equals(t, expectedInst, inst)
 }
