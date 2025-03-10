989f860b6e6172f233abb08b5b3fdd5798a35970
Matt Kirby <MattJKirby@outlook.com>
2025-03-10 23:55:20 +0000

    test gets marshalled correctly

--- ga/flow/flowDefinition.go
+++ ga/flow/flowDefinition.go
@@ -3,15 +3,17 @@ package flow
 import "go-actions/ga/action"
 
 type FlowDefinition struct {
-	actions map[string]*action.ActionInstance
+	Actions map[string]*action.ActionInstance `json:"Actions"`
 }
 
 func NewFlowDefinition() *FlowDefinition {
 	return &FlowDefinition{
-		actions: make(map[string]*action.ActionInstance),
+		Actions: make(map[string]*action.ActionInstance),
 	}
 }
 
 func (fd *FlowDefinition) AddInstance(instance *action.ActionInstance) {
-	fd.actions[instance.Model.ActionUid] = instance
+	fd.Actions[instance.Model.ActionUid] = instance
 }
+
+
--- ga/flow/flowDefinition_test.go
+++ ga/flow/flowDefinition_test.go
@@ -1,6 +1,8 @@
 package flow
 
 import (
+	"encoding/json"
+	"fmt"
 	"go-actions/ga/action"
 	"go-actions/ga/cr/asserts"
 	"go-actions/ga/testing/testHelpers/actionModelTestHelpers"
@@ -13,5 +15,18 @@ func TestAddInstance(t *testing.T) {
 
 	flowDef.AddInstance(instance)
 
-	asserts.Equals(t, instance, flowDef.actions["someInstance:abc"])
+	asserts.Equals(t, instance, flowDef.Actions["someInstance:abc"])
+}
+
+func TestMarshalFlowDefinition(t *testing.T){
+	flowDef := NewFlowDefinition()
+	instance := action.NewActionInstance("someInstance", &actionModelTestHelpers.MockActionModelConfig{MockUid: "abc"})
+
+	flowDef.AddInstance(instance)
+	marshalledInstance, _ := json.Marshal(instance)
+
+	marshalled, err := json.Marshal(flowDef)
+	expected := fmt.Sprintf(`{"Actions":{"%s":%s}}`, instance.Model.ActionUid, marshalledInstance)
+	asserts.Equals(t, nil, err)
+	asserts.Equals(t, expected, string(marshalled))
 }
