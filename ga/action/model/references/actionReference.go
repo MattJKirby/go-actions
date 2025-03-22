a8da4228ac1f825047f6ece545877120b02ac9a5
Matt Kirby <MattJKirby@outlook.com>
2025-03-22 23:19:58 +0000

    add action reference

---
+++ ga/action/model/references/actionReference.go
@@ -0,0 +1,20 @@
+package references
+
+import (
+	"fmt"
+	"go-actions/ga/action/model"
+)
+
+type ActionReference struct {
+	ReferenceUid    string `json:"ReferenceUid"`
+	SourceActionUid string `json:"SourceActionUid"`
+	TargetActionUid string `json:"TargetActionUid"`
+}
+
+func NewActionReference(config model.ActionModelConfig, sourceUid string, targetUid string) *ActionReference {
+	return &ActionReference{
+		ReferenceUid:    fmt.Sprintf("ref:%s", config.GenerateUid()),
+		SourceActionUid: sourceUid,
+		TargetActionUid: targetUid,
+	}
+}
