6e22004c929a023900734d60dc7f04e7c3b3bb12
Matt Kirby <MattJKirby@outlook.com>
2025-04-19 01:03:05 +0100

    need to fix props

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"go-actions/examples"
 	"go-actions/ga"
 )
 
 func main() {
-	definition, _ := ga.GetActionRegistration[*examples.ExampleAction, examples.ExampleActionProps]()
-	action, _ := ga.GetAction[*examples.ExampleAction, examples.ExampleActionProps]()
-	fmt.Println(definition)
-	fmt.Println(action)
+	// definition, _ := ga.GetActionRegistration[*examples.ExampleAction, examples.ExampleActionProps]()
+	// action, _ := ga.GetAction[*examples.ExampleAction, examples.ExampleActionProps]()
+	// fmt.Println(definition)
+	// fmt.Println(action)
 
 	flow := ga.NewFlow()
 	ex1, _ := examples.NewExampleAction(flow, &examples.ExampleActionProps{
@@ -24,7 +22,5 @@ func main() {
 	ex2, _ := examples.NewExampleAction(flow, nil)
 	ex2.Execute()
 
-	val, _ := json.Marshal(action.Instance)
-	fmt.Println(string(val))
 
 }
