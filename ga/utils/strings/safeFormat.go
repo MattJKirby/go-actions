5cac362dc5e805e9fe298ed4f24d356fedd299ab
Matt Kirby <MattJKirby@outlook.com>
2025-04-30 01:11:04 +0100

    created safe format utility

---
+++ ga/utils/strings/safeFormat.go
@@ -0,0 +1,51 @@
+package strings
+
+import "fmt"
+
+func validatePlaceholders(format string, restrictToString bool) (int, error){
+	placeholderCount := 0
+
+	for i := range len(format) {
+		if i + 1 == len(format) {
+			break
+		}
+		
+		if format[i] != '%' {
+			continue
+		}
+
+		if i > 0 && format[i - 1] == '%' {
+			continue
+		}
+
+		if i < len(format) && format[i + 1] == '%' {
+			continue
+		}
+
+		if format[i + 1] != 's' && restrictToString {
+			return 0, fmt.Errorf("string format '%s' contains placeholders other than '%%s'", format)
+		}
+
+		placeholderCount += 1
+	}
+
+	return placeholderCount, nil
+}
+
+func SafeFormat(format string, values ...string) (string, error){
+	placeholderCount, err := validatePlaceholders(format, true)
+	if err != nil {
+		return "", err
+	}
+
+	if len(values) != placeholderCount {
+		return "", fmt.Errorf("invalid number of values provided: expected %d but got %d", placeholderCount, len(values))
+	}
+
+	args := make([]any, len(values))
+	for i, v := range values {
+		args[i] = v
+	}
+
+	return fmt.Sprintf(format, args...), nil
+}
---
+++ ga/utils/strings/safeFormat_test.go
@@ -0,0 +1,114 @@
+package strings
+
+import (
+	"go-actions/ga/utils/testing/assert"
+	"testing"
+)
+
+func TestValidatePlaceholders(t *testing.T) {
+	tests := []struct {
+		name              string
+		format            string
+		restrictToString  bool
+		expectedCount     int
+		expectError       bool
+	}{
+		{
+			name:             "valid %s only",
+			format:           "action:%s:uid:%s",
+			restrictToString: true,
+			expectedCount:    2,
+			expectError:      false,
+		},
+		{
+			name:             "mixed %s and %d with restriction",
+			format:           "action:%s:uid:%d",
+			restrictToString: true,
+			expectedCount:    0,
+			expectError:      true,
+		},
+		{
+			name:             "escaped percent signs",
+			format:           "%%s %%d",
+			restrictToString: true,
+			expectedCount:    0,
+			expectError:      false,
+		},
+		{
+			name:             "non-restricted %d",
+			format:           "id:%d",
+			restrictToString: false,
+			expectedCount:    1,
+			expectError:      false,
+		},
+		{
+			name:             "trailing percent sign",
+			format:           "invalid %",
+			restrictToString: true,
+			expectedCount:    0,
+			expectError:      false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			count, err := validatePlaceholders(tt.format, tt.restrictToString)
+			assert.Equals(t, tt.expectedCount, count)
+			assert.Equals(t, tt.expectError, err != nil)
+		})
+	}
+}
+
+func TestSafeFormat(t *testing.T) {
+	tests := []struct {
+		name        string
+		format      string
+		values      []string
+		expected    string
+		expectError bool
+	}{
+		{
+			name:        "valid format with 2 placeholders",
+			format:      "resource:%s:type:%s",
+			values:      []string{"foo", "bar"},
+			expected:    "resource:foo:type:bar",
+			expectError: false,
+		},
+		{
+			name:        "invalid format with %d",
+			format:      "id:%d",
+			values:      []string{"123"},
+			expected:    "",
+			expectError: true,
+		},
+		{
+			name:        "too few values",
+			format:      "uid:%s:name:%s",
+			values:      []string{"only-one"},
+			expected:    "",
+			expectError: true,
+		},
+		{
+			name:        "too many values",
+			format:      "uid:%s",
+			values:      []string{"val1", "val2"},
+			expected:    "",
+			expectError: true,
+		},
+		{
+			name:        "escaped percent signs",
+			format:      "progress:%% %s",
+			values:      []string{"done"},
+			expected:    "progress:% done",
+			expectError: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := SafeFormat(tt.format, tt.values...)
+			assert.Equals(t, tt.expected, result)
+			assert.Equals(t, tt.expectError, err != nil)
+		})
+	}
+}
